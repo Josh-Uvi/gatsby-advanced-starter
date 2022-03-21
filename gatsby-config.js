4f8cf609cf1c2a8f0f217b0f8ec83a4cec62f5cd
Josh Uvi <joshuauvbiekpahor@gmail.com>
2022-03-21 16:10:43 +0000

    fix: change gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,40 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-theme-amaranth',
-      options: {},
+      resolve: "gatsby-theme-amaranth",
+      options: {
+        basePath: `/blog`,
+        website: {
+          // fbAppId: "1825356251115265",
+          twitterName: "Uvi_jr",
+          url: "https://gatsby-advanced-starter-demo.netlify.com",
+          // googleAnalyticsId: "UA-47311644-5",
+          copyright: "© Copyright 2022 | Josh Uvi",
+          // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter",
+          logoUrl: "/logos/logo.png",
+        },
+
+        user: {
+          id: "JoshUvi",
+          firstName: "Josh",
+          lastName: "Uvi",
+          twitterName: "Uvi_jr",
+          // linkedIn: "your-linkedin",
+          github: "Josh-Uvi",
+          email: "joshuauvbiekpahor@gmail.com",
+          location: "London, United Kingdom",
+          about: "A full-stack web developer looking for a challenge!",
+          // avatar: "https://i.pravatar.cc/300",
+        },
+
+        organization: {
+          name: "Uvi Media",
+          description: "A tech startup company",
+          logoUrl: "/logos/logo.png",
+          url: "https://joshuvi-portfolio.herokuapp.com/",
+        },
+      },
     },
   ],
 };
